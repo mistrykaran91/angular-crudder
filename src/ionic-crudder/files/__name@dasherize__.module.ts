6e3c1eb6bbe5e9cf31fb0cc0a3928a039ac3da64
mistrykaran91 <mistrykaran91@gmail.com>
2020-03-23 10:41:32 +0530

    Small Change

--- src/ionic-crudder/files/__name@dasherize__.module.ts
+++ src/ionic-crudder/files/__name@dasherize__.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-
+import { SharedModule } from '@shared/shared.module';
 import { <%= classify(name) %>PageRoutingModule } from './<%= dasherize(name) %>-routing.module';
 import { <%= classify(name) %>Page } from './<%= dasherize(name) %>.page';
+
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
+    SharedModule,
     <%= classify(name) %>PageRoutingModule
   ],
   declarations: [<%= classify(name) %>Page]
